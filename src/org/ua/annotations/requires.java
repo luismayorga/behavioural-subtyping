8464fdff0c4fee35d5cd87f566e0fd65f0a34cd8
Luis Mayorga <luismayorgam@gmail.com>
2013-10-17 15:49:37 +0200

    Search of annotations

--- src/org/ua/annotations/requires.java
+++ src/org/ua/annotations/requires.java
@@ -5,8 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.CLASS)
-@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.CONSTRUCTOR,
+	ElementType.METHOD})
 public @interface requires {
     String value();
 }
--- src/org/ua/processor/AnnotationProcessor.java
+++ src/org/ua/processor/AnnotationProcessor.java
@@ -9,8 +9,9 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic.Kind;
 
 @SupportedAnnotationTypes({"org.ua.annotations.*"})
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
@@ -18,17 +19,41 @@ public class AnnotationProcessor extends AbstractProcessor {
 
 	private Messager messager;
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public void init(ProcessingEnvironment processingEnv){
-		messager = processingEnv.getMessager();
+	public synchronized void init(ProcessingEnvironment processingEnv){
 		super.init(processingEnv);
+		messager = processingEnv.getMessager();
 	}
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public boolean process(Set<? extends TypeElement> typeElements, RoundEnvironment roundEnvironment) {
-		for (TypeElement typeElement : typeElements) {
+	public boolean process(Set<? extends TypeElement> annotations, 
+			RoundEnvironment roundEnv) {
+		if(!roundEnv.processingOver()){
+			for (TypeElement annotation : annotations) {
+				for(Element el: roundEnv.getElementsAnnotatedWith(annotation)){
+					processClass(el.getEnclosingElement());
+				}
+			}
 		}
 		return true;
+	}
+	/**Processes a class, extracting its structure to an internal representation 
+	 * and stores the contracts within it as well as the relationship with the
+	 * other classes.
+	 * 
+	 * @param element The class to be analyzed
+	 */
+	private void processClass(Element element){
+		if (!element.getKind().equals(ElementKind.CLASS)) {
+			throw new UnsupportedOperationException("The operation is only " +
+					"defined for classes");
+		}
+		//TODO Process class
 	}
 
 }
