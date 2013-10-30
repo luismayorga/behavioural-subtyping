31a0866773278b80ca5ae6d38fb168e2bcd53057
Luis Mayorga <luis@longisland.local>
2013-10-30 23:40:59 +0100

    Retrieval of hierarchy contracts

--- src/org/ua/processor/AnnotationProcessor.java
+++ src/org/ua/processor/AnnotationProcessor.java
@@ -10,14 +10,21 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import javax.tools.Diagnostic.Kind;
 
 @SupportedAnnotationTypes({"org.ua.annotations.*"})
-@SupportedSourceVersion(SourceVersion.RELEASE_6)
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
 public class AnnotationProcessor extends AbstractProcessor {
 
 	private Messager messager;
+	private Types typeUtils;
+	private Elements elementUtils;
 
 	/**
 	 * {@inheritDoc}
@@ -26,6 +33,9 @@ public class AnnotationProcessor extends AbstractProcessor {
 	public synchronized void init(ProcessingEnvironment processingEnv){
 		super.init(processingEnv);
 		messager = processingEnv.getMessager();
+		typeUtils = processingEnv.getTypeUtils();
+		elementUtils = processingEnv.getElementUtils();
+
 	}
 	/**
 	 * {@inheritDoc}
@@ -34,7 +44,9 @@ public class AnnotationProcessor extends AbstractProcessor {
 	public boolean process(Set<? extends TypeElement> annotations, 
 			RoundEnvironment roundEnv) {
 		if(!roundEnv.processingOver()){
+			// Type of annotations present in the analyzed code
 			for (TypeElement annotation : annotations) {
+				// Elements annotated with them
 				for(Element el: roundEnv.getElementsAnnotatedWith(annotation)){
 					processClass(el.getEnclosingElement());
 				}
@@ -48,13 +60,18 @@ public class AnnotationProcessor extends AbstractProcessor {
 	 * 
 	 * @param element The class to be analyzed
 	 */
-	private void processClass(Element element){
-		if (!element.getKind().equals(ElementKind.CLASS)) {
-			//TODO change for an error message showing Bad use of the annotations
-			throw new UnsupportedOperationException("The operation is only " +
-					"defined for classes");
+	private void processClass(Element el){
+		TypeMirror clase = el.asType();
+		//The element is not a class or an interface
+		if (!clase.getKind().equals(TypeKind.DECLARED)) {
+			messager.printMessage(Kind.ERROR,
+					"The annotated element must be a method", el);
+		}else{
+			// Check if there is a superclass
+			DeclaredType dt = (DeclaredType)((TypeElement)el).getSuperclass();
+			boolean iguales = typeUtils.isSameType(elementUtils.getTypeElement("java.lang.Object").asType(), dt);
+			System.out.println("Has superclass:" + !iguales);
 		}
-		//TODO Process class
 	}
 
 }
